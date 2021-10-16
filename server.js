efb8f8be703c333178cbaffe20db8c98a7805c74
AshisPatel <ashis.n.patel@gmail.com>
2021-10-16 09:26:52 -0500

    Add boilerplate server code and folder structure

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// app.search(require('/.routes'));
+
+mongoose.connect(process.env.MONGODB_URI || `mongodb://localhost/social-network-api`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+mongoose.set('debug', true);
+
+app.listen(PORT, () => console.log(`Connected to localhost:${PORT}! (^~^)b`))
