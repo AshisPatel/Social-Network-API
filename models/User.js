a441ce111d9328a4ab28e63b79d44ad578ef597f
AshisPatel <ashis.n.patel@gmail.com>
2021-10-16 10:00:13 -0500

    Add user Model

---
+++ models/User.js
@@ -0,0 +1,38 @@
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    // We are looking for an ObjectId data type, since we are only going to compile the _id of the Thought document in the thoughts array.
+    thoughts: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Thought'
+    }],
+    // References itself, or the UserSchema 
+    friends: [this]
+},
+    {
+
+        toJSON: {
+            virtuals: true
+        },
+        id: false
+
+    }
+)
+
+UserSchema.virtual('friendCount').get(function() {
+    return this.friends.length
+});
+
+const User = model('User', UserSchema);
+
+module.exports = User; 
